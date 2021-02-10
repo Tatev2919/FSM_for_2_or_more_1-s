9eaa141c1dbfd8b174053246462f55bd2f44334a
Tatev2919 <tatev@karapetian.ru>
2021-02-10 17:20:01 +0400

    aa

--- sequence.v
+++ sequence.v
@@ -1,88 +1,69 @@
-module sequence_fsm (
-    input clk,
-    input rst,
-    input data_in,
-    output reg out
-);
-  parameter IDLE = 2'b00;
-  parameter s1 = 2'b01;
-  parameter s2 = 2'b10;
-  parameter s3 = 2'b11;
+module sequence_fsm (clk,rst,data_in,out);
+	input clk,rst,data_in;
+	output reg out;
+ 
+	parameter IDLE = 2'b0,
+		  S1 = 2'b1,
+		  S2 = 2'b10,
+		  S3 = 2'b11;
  
  
-  reg [1:0] state , next_state;
+reg [1:0] state , next_state;
 
-  always @(posedge clk)
-	begin
-      if (rst) begin 
-          state <= IDLE;
-        end
-      else begin 
-   	  state <= next_state;
-      end
-    end
+always @(posedge clk)
+	if (rst)  
+		state <= IDLE;
+	else  
+		state <= next_state;
   
-  always @ (state or data_in)
-        begin 
-          case (state) 
-           IDLE:
-             if(data_in == 1'b1) 
-             begin
-            	next_state = s1; 	
-             end 
-            else 
-              next_state = state;
-            s1: 
-              if(data_in == 1'b0 )
-                begin 
-                    next_state = s2;
-                end
-               else 
-                 begin 
-                    next_state = s3;
-                 end 
-            s2:
-              if(data_in == 1'b1) 
-                begin 
-                    next_state = s1;
-                end
-              else 
-                  begin 
-                      next_state = IDLE;
-                  end
-             s3:
-               if (data_in == 1'b0)
-                 begin 
-                     next_state = s2;
-                 end
-               else 
-                 begin 
-                     next_state = state;
-                 end
-            default: 
-                 next_state  = IDLE;
-          endcase
-        end
+
+always @(state or data_in) 
+	case (state) 
+		IDLE:
+		if(data_in == 1'b1) 
+			next_state = S1; 	 
+		else 
+			next_state = state;
+		S1: 
+			if(data_in == 1'b0 )
+				next_state = S2;
+
+			else 
+				next_state = S3;
+ 
+		S2:
+      			if(data_in == 1'b1) 
+				next_state = S1;
+			else 
+         			next_state = IDLE;
+         
+		S3:
+			if (data_in == 1'b0)
+          			next_state = S2;
+        		else 
+         			next_state = state;
+		default: 
+         		next_state  = IDLE;
+	endcase
+        
   
-  always @(data_in or state)
-    begin
-      case (state)
-        IDLE: out = 0;
-        s1: 
-          if (data_in == 0)
-            out = 0;
-          else 
-            out = 1;
-        s2: 
-          if (data_in == 1)
-            out = 1;
-          else 
-            out = 0;
-        s3:
-          out = 1;
-        default:
-          out = 0;
-      endcase
-    end
+always @(posedge clk)
+	case (state)
+		IDLE: out = 1'b0;
+		S1: 
+			if (data_in == 1'b0)
+				out = 1'b0;
+			else 
+				out = 1'b1;
+		S2: 
+			if (data_in == 1'b1)
+				out = 1'b1;
+			else 
+				out = 1'b0;
+		S3:
+			out = 1'b1;
+		default:
+			out = 1'b0;
+	endcase
 
 endmodule
--- sequence_fsm_tb.v
+++ sequence_fsm_tb.v
@@ -1,59 +1,54 @@
 module sequence_fsm_tb;
-	reg data_in,clk,rst,a,b;
+	reg data_in,clk,rst;
 	wire out;
 	
-  sequence_fsm fsm1(clk, rst,data_in, out);
-	initial 
-      begin 
-        $dumpfile("wv.vcd");
-        $dumpvars();
-      end
-  
-    initial 
-      begin 
-  	  data_in = 0;
-          clk = 0;
-          rst = 1;
-          #20 rst = 0;
-      end
-  
-    always
- 	  begin 	   
-        #10 clk=~clk;
-      end
-  
-   initial 
-     begin 
-        @(posedge clk) data_in <= 1;
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 1; 
-       
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 1;
-        @(posedge clk) data_in <= 0;
-       
-        @(posedge clk) data_in <= 1;
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 0;
-       
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 0;
-        
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 1;
-       
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 1;
-        @(posedge clk) data_in <= 0;
-       
-        @(posedge clk) data_in <= 1;
-        @(posedge clk) data_in <= 0;
-        @(posedge clk) data_in <= 1;
-        @(posedge clk) data_in <= 1;
+sequence_fsm fsm1 (clk, rst,data_in, out);
 
-        #500;
-        $finish;
-      end
+initial begin 
+	$dumpfile("v.vcd");
+	$dumpvars();
+end
+  
+initial begin 
+	data_in = 1'b0;
+	clk = 1'b0;
+	rst = 1'b1;
+	#20 rst = 1'b0;
+end
+  
+always #10 clk=~clk;
+  
+initial begin 
+	@(posedge clk) data_in <= 1'b1;
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b1; 
+
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b1;
+	@(posedge clk) data_in <= 1'b0;
+
+	@(posedge clk) data_in <= 1'b1;
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b0;
+
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b0;
+
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b1;
+
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b1;
+	@(posedge clk) data_in <= 1'b0;
+
+	@(posedge clk) data_in <= 1'b1;
+	@(posedge clk) data_in <= 1'b0;
+	@(posedge clk) data_in <= 1'b1;
+	@(posedge clk) data_in <= 1'b1;
+
+	#500;
+	$finish;
+end
 endmodule
